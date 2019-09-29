1301513619a430cc54abe22fcd535f1e3ce7fb60
ryan-hennigan <ryan.hennigan@tamu.edu>
2019-09-28 22:32:59 -0500

    add heroku postbuild

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const users = require('./routes/api/users');
-
+const path = require('path');
 
 const app = express();
 
@@ -11,6 +11,17 @@ app.use(bodyParser.json());
 
 //Use Routes
 app.use('/api/users', users);
+
+if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+
+}
+
 
 const port = process.env.PORT || 5000;
 
